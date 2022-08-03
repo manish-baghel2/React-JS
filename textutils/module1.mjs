6113b67d631861c44d9ba3236a3755f09072e229
manish-baghel2 <manish_11914130@nitkrr.ac.in>
2022-08-03 13:59:59 +0530

    commit for lecture 6

---
+++ textutils/module1.mjs
@@ -0,0 +1,6 @@
+const a = 54;
+const b = 93;
+const c = 65;
+
+export default a;
+export {a, b, c};
---
+++ textutils/module2.mjs
@@ -0,0 +1,6 @@
+import dsa, {a, b, c} from './module1.mjs'
+
+console.log(dsa);
+console.log(a);
+console.log(b);
+console.log(c);
